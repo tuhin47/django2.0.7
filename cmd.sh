e47a1eb761f4d4c59aa661582606cebfd0d9ba99
tuhin47 <tuhintowhidul9@gmail.com>
2019-04-17 16:24:47 +0600

    9 - Create Product Objects in the Python Shell

--- cmd.sh
+++ cmd.sh
@@ -4,3 +4,10 @@
 
 python manage.py makemigrations
 python manage.py migrate
+
+#9 - Create Product Objects in the Python Shell
+
+python manage.py Shell
+>>from Products.models import Product
+>>Product.objects.all()
+>>Product.objects.create(title="T2",description="D2",price="P2",summary="S2")
